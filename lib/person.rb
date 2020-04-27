91a3f632928e9aea5fb44a434476445d7522575a
John Carter <johncarter352@gmail.com>
2020-04-27 04:43:51 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,15 +3,15 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-#def initialize(attributes)
-  #attributes.each{|key, value| self.send(("#{key=}"), value)}
-#end
-
 def initialize(attributes)
-    attributes.each do |key, value|
-      self.send(("#{key}="), value)
-    end
-  end
+  attributes.each{|key, value| self.send(("#{key=}"), value)}
+end
+
+#def initialize(attributes)
+    #attributes.each do |key, value|
+      #self.send(("#{key}="), value)
+    #end
+  #end
 
   
 end
